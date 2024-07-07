b5dc478e69517a4cee4eba51fae3bfba374f2abd
ankitjain0748 <ankitjain0748@gmail.com>
2024-07-07 12:50:07 +0530

    some changement1

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
-const apiroute =require("./Routes/ContactUs")
+const apiroute = require("./Routes/ContactUs")
 
-app.use("/api",apiroute);
-app.get("/",(req,res)=>{
-    console.log("heelo world !!")
-})
+app.use("/api", apiroute);
+
+
+app.get('/', (req, res) => {
+    res.json({ message: 'API data response' });
+});
+
 const mongoose = require('mongoose')
 mongoose.connect("mongodb://localhost:27017/contact")
 
-app.listen(5000,()=>{
+app.listen(5000, () => {
     console.log("Server is running on port 5000")
 })
